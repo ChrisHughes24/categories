cd203c6c56f73fb93e9b16104b7e88104129b399
ChrisHughes24 <chrishughes24@gmail.com>
2023-01-30 11:39:10 +0000

    add Emb and Proj

--- Categories/SecondAttempt.lean
+++ Categories/SecondAttempt.lean
@@ -46,15 +46,49 @@ open Obj
 nonrec def Obj.app (C D : Cat) (h : C ≤ D) (X : Obj C) : Obj D :=
   notCorepr (NotCoreprObj.app C D h X)
 
+nonrec def Obj.coprod {C : Cat} (X Y : Obj C) : Obj C :=
+  corepr (CoreprObj.coprod X Y)
+
+nonrec def Obj.prod {C : Cat} (X Y : Obj C) : Obj C :=
+  notCorepr (NotCoreprObj.repr C (ReprObj.prod X Y))
+
+nonrec def Obj.LAdj (C D : Cat) (h : C ≤ D) (H : HasLAdj C D h) (X : Obj D) : Obj C :=
+  corepr (CoreprObj.LAdj C D h H X)
+
+nonrec def Obj.RAdj (C D : Cat) (h : C ≤ D) (H : HasRAdj C D h) (X : Obj D) : Obj C :=
+  notCorepr (NotCoreprObj.repr C (ReprObj.RAdj C D h H X))
+
+nonrec def Obj.bot (C : Cat) : Obj C :=
+  corepr (CoreprObj.bot C)
+
+inductive Emb {C : Cat} : Obj C → Obj C → Type
+| inl : ∀ {X Y : Obj C}, Emb X (coprod X Y)
+| inr : ∀ {X Y : Obj C}, Emb Y (coprod X Y)
+| unit : ∀ {D : Cat} (h : D ≤ C) (H : HasLAdj D C h) {X : Obj C},
+    Emb X (app D C h (LAdj D C h H X))
+
+inductive Proj {C : Cat} : Obj C → Obj C → Type
+| fst : ∀ {X Y : Obj C}, Proj (prod X Y) X
+| snd : ∀ {X Y : Obj C}, Proj (prod X Y) Y
+| counit : ∀ {D : Cat} (h : D ≤ C) (H : HasRAdj D C h) {X : Obj C},
+    Proj (app D C h (RAdj D C h H X)) X
+
 mutual
 
 inductive HomCorepr : {C : Cat} → CoreprObj C → Obj C → Type
   | coprod {C : Cat} {X Y Z : Obj C} (f : Hom X Z) (g : Hom Y Z) : HomCorepr (CoreprObj.coprod X Y) Z
-  | LAdj {C D : Cat} (h : C ≤ D) (H : HasLAdj C D h) {X : Obj D} {Y : Obj C}
+  | ladj {C D : Cat} (h : C ≤ D) (H : HasLAdj C D h) {X : Obj D} {Y : Obj C}
       (f : Hom X (app C D h Y)) : HomCorepr (CoreprObj.LAdj C D h H X) Y
   | bot {C : Cat} {X : Obj C} : HomCorepr (CoreprObj.bot C) X
 
 inductive HomRepr : ∀ {C : Cat}, NotCoreprObj C → ReprObj C → Type
+  /-- Never to be used when `f` and `g` are of the form `f = f₁ ; f₂`, `g = g₁ ; g₂` and
+  `f₁ = g₁` -/
+  | prod {C : Cat} {X : NotCoreprObj C} {Y Z : Obj C}
+    (f : Hom (notCorepr X) Y) (g : Hom (notCorepr X) Z) : HomRepr X (ReprObj.prod Y Z)
+  /-- Never to be used when `f` is a composition. -/
+  | radj {C D : Cat} (h : C ≤ D) (H : HasRAdj C D h) {X : NotCoreprObj C} {Y : Obj D}
+    (f : Hom (app C D h (notCorepr X)) Y) : HomRepr X (ReprObj.RAdj C D h H Y)
 
 inductive Hom : {C : Cat} → Obj C → Obj C → Type
 
