1f715096c71ceb3a3b2b37587d13b12743ee1b8e
ChrisHughes24 <chrishughes24@gmail.com>
2023-01-30 10:09:00 +0000

    small progress

--- Categories/SecondAttempt.lean
+++ Categories/SecondAttempt.lean
@@ -10,6 +10,8 @@ variable {Cat : Type} [CatSystem Cat]
 
 open CatSystem
 
+--TODO : think about how adjoint preserve products.
+
 mutual
 
 inductive CoreprObj : Cat → Type
@@ -21,12 +23,17 @@ inductive ReprObj : Cat → Type
   | prod {C : Cat} : Obj C → Obj C → ReprObj C
   | RAdj (C D : Cat) (h : C ≤ D) : HasRAdj C D h → Obj D → ReprObj C
 
+inductive NotCoreprObj : Cat → Type
+  | var : (C : Cat) → ℕ → NotCoreprObj C
+  /- Below constructor should never be applied to product when `C ⥤ D` has a LAdj or
+  to coproduct when `C ⥤ D` has a RAdj. -/
+  | app : ∀ (C D : Cat), C ≤ D → Obj C → NotCoreprObj D
+  | repr : ∀ (C : Cat), ReprObj C → NotCoreprObj C
+  | top : (C : Cat) → NotCoreprObj C
+
 inductive Obj : Cat → Type
-  | var : (C : Cat) → ℕ → Obj C
-  | app : ∀ (C D : Cat), C ≤ D → Obj C → Obj D
-  | repr : ∀ (C : Cat), ReprObj C → Obj C
-  | corepr : ∀ (C : Cat), CoreprObj C → Obj C
-  | top : (C : Cat) → Obj C
+  | corepr : ∀ {C : Cat}, CoreprObj C → Obj C
+  | notCorepr : ∀ {C : Cat}, NotCoreprObj C → Obj C
 
 end
 
@@ -34,17 +41,20 @@ end
 The exception to the rule being that the normal form of a map into `top` is always `top_mk` unless the
 map is from `bot`. -/
 
-open Obj CoreprObj ReprObj
+open Obj
+
+nonrec def Obj.app (C D : Cat) (h : C ≤ D) (X : Obj C) : Obj D :=
+  notCorepr (NotCoreprObj.app C D h X)
 
 mutual
 
 inductive HomCorepr : {C : Cat} → CoreprObj C → Obj C → Type
-  | coprod {C : Cat} {X Y Z : Obj C} (f : Hom X Z) (g : Hom Y Z) : HomCorepr (coprod X Y) Z
+  | coprod {C : Cat} {X Y Z : Obj C} (f : Hom X Z) (g : Hom Y Z) : HomCorepr (CoreprObj.coprod X Y) Z
   | LAdj {C D : Cat} (h : C ≤ D) (H : HasLAdj C D h) {X : Obj D} {Y : Obj C}
-      (f : Hom X (app C D h Y)) : HomCorepr (LAdj C D h H X) Y
-  | bot {C : Cat} {X : Obj C} : HomCorepr (bot C) X
+      (f : Hom X (app C D h Y)) : HomCorepr (CoreprObj.LAdj C D h H X) Y
+  | bot {C : Cat} {X : Obj C} : HomCorepr (CoreprObj.bot C) X
 
---inductive HomRepr : ∀ {C : Cat}, CoreprObj C → Obj C → Type
+inductive HomRepr : ∀ {C : Cat}, NotCoreprObj C → ReprObj C → Type
 
 inductive Hom : {C : Cat} → Obj C → Obj C → Type
 
